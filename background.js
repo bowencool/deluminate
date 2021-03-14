f0c5e2c44bead696ebdedd83b16470d5b186425e
Bowen <z.bowen66@gmail.com>
2021-03-14 21:24:42 +0800

    style

--- background.js
+++ background.js
@@ -105,15 +105,13 @@ function init() {
 
   chrome.runtime.onMessage.addListener(
       function(request, sender, sendResponse) {
-        console.log(
-          // request,
-          // window.matchMedia('(prefers-color-scheme: dark)').matches,
-          getEnabled(),
-        )
+        // console.log(
+        //   // request,
+        //   // window.matchMedia('(prefers-color-scheme: dark)').matches,
+        //   getEnabled(),
+        // )
         if ('set_enabled' in request) {
           if (getEnabled() !== request['set_enabled']) {
-            // setEnabled(true)
-            // updateTabs();
             toggleEnabled();
           }
         }
